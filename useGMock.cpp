0124ac0a0d1c1c263e2a326ae561b6a5b49cf15b
Knitschi <knitschi@freenet.de>
2017-10-23 19:41:50 +0200

    The testproject can now be build with VS 2015

--- useGMock.cpp
+++ useGMock.cpp
@@ -8,18 +8,18 @@ public:
     {
         return a * b;
     }
-}
+};
 
 class MyMock : public MyClass
 {
 public:
-    MOCK_METHOD2(multiply, double(double,double))
-}
+	MOCK_METHOD2(multiply, double(double, double));
+};
 
 TEST( Test1, Test1)
 {
     MyMock mock;
-    EXPECT_CALL(mock, multiply).WillOnce(testing::Return(5));
+	EXPECT_CALL(mock, multiply(testing::_, testing::_)).WillOnce(testing::Return(5));
 
     EXPECT_EQ( mock.multiply(2,1), 5);
 }
