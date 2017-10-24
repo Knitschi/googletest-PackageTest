8f9002d9d504a53a4da4220bcd59cc00e5685649
Knitschi <knitschi@freenet.de>
2017-10-24 08:30:38 +0200

    Make sure the job is executed on a node

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,9 +4,11 @@
 This is a .groovy script that starts builds of the googletest-PackageTest project for multiple configurations.
 */
 
-stage('Trigger Builds')
+stage('Run Builds')
 {
-    build job: 'CMakeProjectBuildJob', parameters: [
+    node('master')
+    {
+        build job: 'CMakeProjectBuildJob', parameters: [
         string(name: 'RepositoryUrl', value: 'https://github.com/Knitschi/googletest-PackageTest.git'), 
         string(name: 'Googletest-VS2015-static-debug', value: 'GoogleTestPackageTest'), 
         string(name: 'BuildSlaveTag', value: 'Windows-10'), 
@@ -21,4 +23,26 @@ stage('Trigger Builds')
         string(name: 'AdditionalGenerateArguments', value: '-G"Visual Studio 14 2015"'), 
         string(name: 'AdditionalBuildArguments', value: '--config Release')
         ]
+    }
 }
+
+/**
+stage('Build Pipeline')
+{
+    def parallelNodes = [:]
+    parallelNodes.failFast = true
+    
+    // add nodes for building the pipeline
+    for(toolchain in toolchains)
+    {
+        echo "Create build node " + toolchain
+        def myNode = createBuildNode(toolchain)
+        parallelNodes[toolchain] = myNode
+    }
+
+    // run the nodes
+    parallel parallelNodes
+}
+*/
+
+
